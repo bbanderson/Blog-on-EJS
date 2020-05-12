b881bdcf108a8661ae38e9b51279dcbc82ed0594
bbanderson <tombyun@naver.com>
2020-05-12 22:14:16 +0900

    Add heroku port

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.get('/posts/:category', function (req, res) {
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
