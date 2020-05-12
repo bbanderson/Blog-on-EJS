9a60cacd1db4b036a8341f1be14df8908b78aab3
bbanderson <tombyun@naver.com>
2020-05-12 14:20:51 +0900

    Render posts on home route

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const posts = []
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.render('home', {homeStartingContent: homeStartingContent})
+  res.render('home', {homeStartingContent: homeStartingContent, posts: posts})  
 })
 
 app.get('/about', function (req, res) {
@@ -36,9 +38,8 @@ app.post('/compose', function (req, res) {
     title: req.body.title,
     post: req.body.post
   }
-  console.log(post);
-  
-  res.redirect('/compose')
+  posts.push(post)  
+  res.redirect('/')
 })
 
 
