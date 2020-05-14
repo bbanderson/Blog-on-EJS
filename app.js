2f578c8fe82360ed500849b2abb8959b87730d52
bbanderson <tombyun@naver.com>
2020-05-14 16:23:14 +0900

    Fetch mongoose

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin-bbanderson:1111@cluster0-v9rqf.mongodb.net/composeDB', {useNewUrlParser:true, useUnifiedTopology:true})
+
+const composeSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Article = new mongoose.model('Article', composeSchema)
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = []
+// const posts = []
 
 const app = express();
 
@@ -19,7 +29,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.render('home', {homeStartingContent: homeStartingContent, posts: posts})  
+  Article.find((err, articles) => {
+    res.render('home', {homeStartingContent: homeStartingContent, posts: articles})  
+  })
 })
 
 app.get('/about', function (req, res) {
@@ -35,23 +47,32 @@ app.get('/compose', function (req, res) {
 })
 
 app.post('/compose', function (req, res) {
-  const post = {
+  const article = new Article({
     title: req.body.title,
-    post: req.body.post
-  }
-  posts.push(post)  
+    content: req.body.post
+  })
+
+  article.save()
+
   res.redirect('/')
 })
 
-app.get('/posts/:category', function (req, res) {
+app.get('/posts/:id', function (req, res) {
   // console.log(req.body);
-  
-  posts.forEach(element => {
-    if (_.lowerCase(element.title) === _.lowerCase(req.params.category))
-    {
-      res.render('post', {post: element})
+  Article.findById({_id: req.params.id}, (err, article) => {
+    if (!err) {
+      res.render('post', {post: article})
+    } else {
+      console.log(err);
+      
     }
   })
+  // posts.forEach(element => {
+  //   if (_.lowerCase(element.title) === _.lowerCase(req.params.category))
+  //   {
+  //     res.render('post', {post: element})
+  //   }
+  // })
   // console.log(req.params.category);
   
 })
